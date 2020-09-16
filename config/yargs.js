4a52beb4f1fafa61efdec315f3e3e63bb07d7d10
Jesus Hdez <Jessiehdez36@hotmail.com>
2020-09-15 20:02:46 -0500

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,26 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+
+const argv = require('yargs')
+    .command('crear', 'Crea un elemento por hacer', { descripcion })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', { descripcion, completado })
+    .command('borrar', 'Elimina una tarea', { descripcion })
+    .command('listar', 'Muestra un listado de depende si estan completados o no', { completado })
+    .help()
+    .argv;
+
+
+
+module.exports = {
+    argv
+}
